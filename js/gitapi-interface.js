0d357f7c6eb7b69a9e8be93658887a54de8512d4
brianlaw033 <brian9328hk@gmail.com>
2017-02-17 11:57:03 +0800

    adding a table

--- js/gitapi-interface.js
+++ js/gitapi-interface.js
@@ -1,15 +1,30 @@
 var Githubapp = require('./../js/gitapi.js').githubappModule;
 var githubObject = new Githubapp();
-
+var mode = "";
 var displayInfo = function(response){
-  $('#name').html('Login Name: '+ response.login);
-  $('#link').html('Link: '+ response.url);
+  if (mode === ''){
+    $('#loginName').html('Login Name: '+ response.login);
+    $('#link').html('Link: '+ response.url);
+  }else if (mode === '/repos'){
+    var reposArray = response;
+    for (i=0;i<reposArray.length; i++){
+      $('#reposname').append('<li>'+ reposArray[i].name + '</li>');
+      $('#reposdate').append('<li>'+ moment(reposArray[i].created_at).format('LLL')+ '</li>');
+    }
+    console.log(reposArray[1].name);
+  }
 };
 
 $(document).ready(function(){
-  $('#btn').click(function(){
+  $('.btn').click(function(){
     var username = $('#username').val();
     $('#username').val('');
-    githubObject.getGithubInfo(username, displayInfo);
-  })
+    if(this.id === 'basic'){
+      mode = '';
+    }else if (this.id === 'reposDate') {
+      mode = '/repos';
+    }
+
+    githubObject.getGithubInfo(username, displayInfo, mode);
+  });
 });
--- js/gitapi.js
+++ js/gitapi.js
@@ -2,8 +2,8 @@ var apiKey = require('./../.env').apiKey;
 var Githubapp = function(){
 };
 
-Githubapp.prototype.getGithubInfo = function(username, displayFunction){
-  $.get('https://api.github.com/users/'+username+'?access_token='+apiKey).then(function(response){
+Githubapp.prototype.getGithubInfo = function(username, displayFunction, mode){
+  $.get('https://api.github.com/users/' + username + mode + '?access_token=' + apiKey).then(function(response){
     displayFunction(response);
   }).fail(function(error){
     displayFunction(error.responseJSON.message);
